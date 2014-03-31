108ae05e36190b517c8f11585390f254b4252e8d
Daniel P. Clark <webmaster@6ftdan.com>
2014-03-31 14:11:20 -0400

    comments

--- rtsingle.rb
+++ rtsingle.rb
@@ -2,6 +2,7 @@
 
 # RTsingle - ReTweet a single account to your twitter feed.  Uses oldschool RT @user.
 
+
 require 'twitter'
 require 'credentials' # put your API keys in here
 
@@ -15,22 +16,21 @@ end
 to_retweet = client.user_timeline(RT_ACCOUNT)
 to_self = client.user_timeline(OWNER)
 
-my_tweets = Array.new
-
-def charlimiter(text_in)
+def charlimiter(text_in) # 140 Char max!
 	if text_in.length > 140
 		return (text_in[0..136] + '...')
 	end
 end
 
+my_tweets = Array.new
 
-to_self.each do |object|
+to_self.each do |object| # get your own tweets for the last 24 hours
     if ((object.created_at - Time.now).to_i / (24 * 60 * 60) + 1) == 0
 		my_tweets.push(object.text) if object.is_a?(Twitter::Tweet)
     end
 end
 
-to_retweet.each do |object|
+to_retweet.each do |object| # tweets of last 24 hours are retweeted with no duplicates
     if ((object.created_at - Time.now).to_i / (24 * 60 * 60) + 1) == 0
 		if object.is_a?(Twitter::Tweet) and not object.retweet? and not my_tweets.include?(charlimiter("RT @#{RT_ACCOUNT} " + object.text))
 			client.update(charlimiter("RT @#{RT_ACCOUNT} " + object.text))
